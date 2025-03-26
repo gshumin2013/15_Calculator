511d7f0ce59322218a6f927e6b81d42245991e76
shumingleb <shumingleb@gmail.com>
2025-03-26 15:44:47 +0500

    docker dompose ports

---
+++ 15_Calculator/Migrations/20250326103711_m_init1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _15_Calculator.Migrations
+{
+    public partial class m_init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
